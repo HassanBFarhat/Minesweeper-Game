095744500bf13e8fa32dd77ce0060fbb5d13cf85
HassanBFarhat <farhathassan@rocketmail.com>
2024-01-24 23:09:24 -0800

    implemented more functions into main.js, still broken will fix later

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let cellSize = 50;
 let canvas = document.getElementById("canvas");
 canvas.width = width * cellSize;
 canvas.height = height * cellSize;
+let ctx = canvas.getContext("2d");
+
 
 function Cell (x, y) {
     this.x = x;
@@ -15,23 +17,56 @@ function Cell (x, y) {
     this.open = false;
 };
 
+
 Cell.prototype.draw = (ctx, minefield) => {
     ctx.save();
-    
-
-
+    ctx.translate(this.x * cellSize, this.y * cellSize);
+    ctx.fillStyle = this.open ? "#eee" : "#bbb";
+    ctx.fillRect(1, 1, cellSize - 2, cellSize - 2);
+    let str = "";
+    if (this.flagged) {
+        ctx.fillStyle = "#00f";
+        str = String.fromCharCode(9873);
+    } else if (this.open) {
+        let bombsInArea = this.cellsAroundBomb(minefield).filter(c => c.bomb).length;
+        if (this.bomb) {
+            ctx.fillStyle = "#f00";
+            str = "*";
+        } else if (bombsInArea > 0) {
+            ctx.fillStyle = "#000";
+            str = bombsInArea;
+        }
+    }
+    ctx.textAlign = "center";
+    ctx.font = "20px Verdana";
+    ctx.fillText(s, cellSize/2, cellSize/2 + 10);
     ctx.restore();
 };
 
 
-
-
-
-
+Cell.prototype.cellsAroundBomb = (field) => {
+    let arr = [];
+    for (let yy = -1; yy <= 1; yy++) {
+        let arrY = this.y + yy;
+        if (arrY < 0 || arrY >= height) {
+            continue;
+        }
+        for (let xx = -1; xx <= 1; xx++) {
+            if (xx == 0 && yy == 0) {
+                continue;
+            }
+            let arrX = this.x + xx;
+            if (arrX < 0 || arrX >= width) {
+                continue;
+            }
+            arr.push(field[arrY][arrX]);
+        }
+    }
+    return arr;
+};
 
 
 let gameField = init();
-
 function init() {
     let gameArray = [];
     for (let i = 0; i < height; i++) {
@@ -51,4 +86,27 @@ function init() {
             }
         }
     }
+    return gameArray;
 };
+
+
+function draw() {
+    eachCell(cell => cell.draw(ctx, field));
+};
+
+function openAll() {
+    eachCell(cell => cell.open = true);
+};
+
+openAll();
+
+function eachCell(fxn) {
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            fxn(field[i][j]);
+        }
+    }
+};
+
+
+draw();
